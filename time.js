80a5a4a51b26161e8e5b3048110f48142674cb34
Wendy Edwards <mrscake@gmail.com>
2017-02-21 11:50:27 -0600

    http to https in eonet url

--- time.js
+++ time.js
@@ -457,7 +457,7 @@ $(function() {
         }
         // Search by category
         else {
-            eonet_url = "http://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/" + eonet_cat + "?days=" + eonet_day_range;
+            eonet_url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/" + eonet_cat + "?days=" + eonet_day_range;
         }
         $.getJSON(eonet_url, function(data){
             var items = [];
